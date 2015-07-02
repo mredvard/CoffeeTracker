6fe7a21341ea9a2afef894e3323ab503533529de
Edgar Contreras <edgar@nochesdecafeina.com>
2015-07-03 00:28:52 +0200

    Testing new distro

--- coffeetracker/urls.py
+++ coffeetracker/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     # Local apps
     url(r'^$', TemplateView.as_view(
         template_name="website/home.html"), name='home'),
-    url(r'^webapp/$',
-        include('webapp.urls', namespace='webapp', app_name='webapp')),
+    url(r'^welcomeme/$',
+        include('welcomeme.urls', namespace='welcomeme', app_name='welcomeme')),
     url(r'^coffeemaker/',
         include('coffeemaker.urls', namespace='coffeemaker', app_name='coffeemaker')),
 
--- settings/dev_settings.py
+++ settings/dev_settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = (
     'coffeemaker',
     'webapp',
     'website',
+    'welcomeme',
 )
 
 MIDDLEWARE_CLASSES = (
